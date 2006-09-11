931329e29f584eb7732ec9d31247a4da56b4a30e
liberatorusa <liberatorusa@9712d391-321d-0410-84f4-d6a0ebf28296>
2006-09-11 19:52:43 +0000

    Added items remotely
    G:\SVNWORK\GUALL\GUCEF\gucefVFS\include\gucefVFSDLLInit.h
    G:\SVNWORK\GUALL\GUCEF\gucefVFS\include\CFilePackage.h
    G:\SVNWORK\GUALL\GUCEF\gucefVFS\include\CFPCodec.h
    G:\SVNWORK\GUALL\GUCEF\gucefVFS\include\CFPCodecPlugin.h
    G:\SVNWORK\GUALL\GUCEF\gucefVFS\include\CFPCodecPluginManager.h
    G:\SVNWORK\GUALL\GUCEF\gucefVFS\include\CFPCodecPluginRef.h
    G:\SVNWORK\GUALL\GUCEF\gucefVFS\include\CFPCodecRegistry.h
    G:\SVNWORK\GUALL\GUCEF\gucefVFS\include\CGUCEFVFSModule.h
    G:\SVNWORK\GUALL\GUCEF\gucefVFS\include\CVFS.H
    G:\SVNWORK\GUALL\GUCEF\gucefVFS\include\CVFSHandle.h
    G:\SVNWORK\GUALL\GUCEF\gucefVFS\include\CVFSURLHandler.h
    G:\SVNWORK\GUALL\GUCEF\gucefVFS\include\CVFSURLResource.h
    G:\SVNWORK\GUALL\GUCEF\gucefVFS\include\gucefVFS.h
    G:\SVNWORK\GUALL\GUCEF\gucefVFS\include\gucefVFS_config.h
    G:\SVNWORK\GUALL\GUCEF\gucefVFS\include\gucefVFS_ETypes.h
    G:\SVNWORK\GUALL\GUCEF\gucefVFS\include\gucefVFS_macros.h

---
+++ gucefVFS/include/gucefVFS.h
@@ -0,0 +1,86 @@
+/*
+ * Copyright (C) Dinand Vanvelzen. 2002 - 2004.  All rights reserved.
+ *
+ * All source code herein is the property of Dinand Vanvelzen. You may not sell
+ * or otherwise commercially exploit the source or things you created based on
+ * the source.
+ *
+ * THE SOFTWARE IS PROVIDED "AS-IS" AND WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY 
+ * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
+ * IN NO EVENT SHALL DINAND VANVELZEN BE LIABLE FOR ANY SPECIAL, INCIDENTAL, 
+ * INDIRECT OR CONSEQUENTIAL DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER 
+ * RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER OR NOT ADVISED OF 
+ * THE POSSIBILITY OF DAMAGE, AND ON ANY THEORY OF LIABILITY, ARISING OUT 
+ * OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE. 
+ */
+
+/*
+ *      This is the main include header for the gucefCORE library.
+ *      It is part of a framework called GUCEF.
+ *      All classes in this library have to do core framework functionality
+ *      and many of the other modules will have a dependancy on this module.
+ *      It houses the generic toolkits.
+ */
+
+#ifndef GUCEFVFS_H
+#define GUCEFVFS_H
+
+/*-------------------------------------------------------------------------//
+//                                                                         //
+//      INCLUDES                                                           //
+//                                                                         //
+//-------------------------------------------------------------------------*/
+
+/*
+ *      Library build configuration      
+ */
+#ifndef GUCEFVFS_CONFIG_H
+#include "gucefVFS_config.h"
+#define GUCEFVFS_CONFIG_H
+#endif /* GUCEFVFS_CONFIG_H ? */
+
+/*
+ *      Library specific macros and generic macros
+ */
+#ifndef GUCEFVFS_MACROS_H
+#include "gucefVFS_macros.h"
+#define GUCEFVFS_MACROS_H
+#endif /* GUCEFVFS_MACROS_H ? */
+
+/*
+ *      Simple types
+ */
+#ifndef ETYPES_H
+#include "ETypes.h"
+#define ETYPES_H
+#endif /* ETYPES_H ? */
+
+/*---------------------------------------------------------------------------/
+
+        C++ includes
+
+/---------------------------------------------------------------------------*/
+#ifdef __cplusplus
+
+#ifndef CVFS_H
+#include "CVFS.h"
+#define CVFS_H
+#endif /* CVFS_H ? */
+
+#endif /* __cplusplus ? */
+/*--------------------------------------------------------------------------*/
+
+#endif /* GUCEFCORE_H ? */
+
+/*-------------------------------------------------------------------------//
+//                                                                         //
+//      Info & Changes                                                     //
+//                                                                         //
+//-------------------------------------------------------------------------//
+
+- 11-03-2004 :
+       - Made the initial version of the library header. The classes here
+         where originally part of one large GUCEF codebase without segmentation.
+
+-----------------------------------------------------------------------------*/
