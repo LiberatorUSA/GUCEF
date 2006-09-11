4f76834ba5ee90bf2e2d44ecba2fc44f311ece28
liberatorusa <liberatorusa@9712d391-321d-0410-84f4-d6a0ebf28296>
2006-09-11 19:53:26 +0000

    Added items remotely
    G:\SVNWORK\GUALL\GUCEF\gucefVFS\include\gucefVFSDLLInit.h
    G:\SVNWORK\GUALL\GUCEF\gucefVFS\include\CFilePackage.h
    G:\SVNWORK\GUALL\GUCEF\gucefVFS\include\CFPCodec.h
    G:\SVNWORK\GUALL\GUCEF\gucefVFS\include\CFPCodecPlugin.h
    G:\SVNWORK\GUALL\GUCEF\gucefVFS\include\CFPCodecPluginManager.h
    G:\SVNWORK\GUALL\GUCEF\gucefVFS\include\CFPCodecPluginRef.h
    G:\SVNWORK\GUALL\GUCEF\gucefVFS\include\CFPCodecRegistry.h
    G:\SVNWORK\GUALL\GUCEF\gucefVFS\include\CGUCEFVFSModule.h
    G:\SVNWORK\GUALL\GUCEF\gucefVFS\include\CVFS.H
    G:\SVNWORK\GUALL\GUCEF\gucefVFS\include\CVFSHandle.h
    G:\SVNWORK\GUALL\GUCEF\gucefVFS\include\CVFSURLHandler.h
    G:\SVNWORK\GUALL\GUCEF\gucefVFS\include\CVFSURLResource.h
    G:\SVNWORK\GUALL\GUCEF\gucefVFS\include\gucefVFS.h
    G:\SVNWORK\GUALL\GUCEF\gucefVFS\include\gucefVFS_config.h
    G:\SVNWORK\GUALL\GUCEF\gucefVFS\include\gucefVFS_ETypes.h
    G:\SVNWORK\GUALL\GUCEF\gucefVFS\include\gucefVFS_macros.h

---
+++ gucefVFS/include/gucefVFS_macros.h
@@ -0,0 +1,359 @@
+/*
+ * Copyright (C) Dinand Vanvelzen. 2002 - 2005.  All rights reserved.
+ *
+ * All source code herein is the property of Dinand Vanvelzen. You may not sell
+ * or otherwise commercially exploit the source or things you created based on
+ * the source.
+ *
+ * THE SOFTWARE IS PROVIDED "AS-IS" AND WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY 
+ * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
+ * IN NO EVENT SHALL DINAND VANVELZEN BE LIABLE FOR ANY SPECIAL, INCIDENTAL, 
+ * INDIRECT OR CONSEQUENTIAL DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER 
+ * RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER OR NOT ADVISED OF 
+ * THE POSSIBILITY OF DAMAGE, AND ON ANY THEORY OF LIABILITY, ARISING OUT 
+ * OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE. 
+ */
+
+#ifndef GUCEFVFS_MACROS_H
+#define GUCEFVFS_MACROS_H
+
+/*
+ *      Build configuration specific macros.
+ *      Also includes the generic macros which are build config independant.
+ */
+
+/*-------------------------------------------------------------------------//
+//                                                                         //
+//      INCLUDES                                                           //
+//                                                                         //
+//-------------------------------------------------------------------------*/
+
+#ifndef MACROS_H
+#include "macros.h"                /* generic often used macros */
+#define MACROS_H
+#endif /* MACROS_H ? */
+
+#ifndef GUCEFVFS_ETYPES_H
+#include "gucefVFS_ETypes.h"      /* simple types */
+#define GUCEFVFS_ETYPES_H
+#endif /* GUCEFVFS_ETYPES_H ? */
+
+#ifndef GUCEFVFS_CONFIG_H
+#include "gucefVFS_config.h"      /* Module build configuration */
+#define GUCEFVFS_CONFIG_H
+#endif /* GUCEFVFS_CONFIG_H ? */
+
+/*-------------------------------------------------------------------------//
+//                                                                         //
+//      GENERAL MACROS                                                     //
+//                                                                         //
+//-------------------------------------------------------------------------*/
+
+/*
+ *      O/S Detection macro.
+ *      If you don't want to use the auto detection macro then set
+ *      the MANUAL_OS_DEFINE define in the config file.
+ */
+#ifndef MANUAL_OS_DEFINE
+  #if defined( WIN32 ) || defined( _WIN32 )
+    #define MSWIN_BUILD
+  #elif defined( linux )
+    #define LINUX_BUILD
+  #else
+    #error Cannot automaticly detect your operating system
+  #endif  
+#endif /* MANUAL_OS_DEFINE ? */
+
+/*-------------------------------------------------------------------------*/
+
+/*
+ *      Auto detection of debug mode
+ */
+#ifndef MANUAL_DEBUG_MODE_DEFINE
+  #if defined( _DEBUG )
+    #define DEBUG_MODE
+  #else
+    #undef DEBUG_MODE
+  #endif /* compiler DEBUG switches */
+#endif /* MANUAL_DEBUG_MODE_DEFINE ? */
+
+/*-------------------------------------------------------------------------*/
+
+/*
+ *      In debug mode we will include the core module's tracer
+ */
+#ifdef __cplusplus
+  #ifdef DEBUG_MODE
+    #ifndef CTRACER_H
+      #include "CTracer.h"
+      #define CTRACER_H
+    #endif /* CTRACER_H ? */
+  #else
+    #define TRACE
+  #endif
+#endif /* __cplusplus ? */
+
+/*-------------------------------------------------------------------------*/
+
+/*
+ *      Macro for enabling/disabling the GUCEF namespace
+ */
+#undef GUCEF_NAMESPACE_BEGIN
+#undef GUCEF_NAMESPACE_END   
+#ifdef USE_GUCEF_NAMESPACE
+  #define GUCEF_NAMESPACE_BEGIN   namespace GUCEF {
+  #define GUCEF_NAMESPACE_END     }
+#else
+  #define GUCEF_NAMESPACE_BEGIN
+  #define GUCEF_NAMESPACE_END
+#endif /* USE_GUCEF_NAMESPACE ? */
+
+/*-------------------------------------------------------------------------*/
+
+/*
+ *      Macro for enabling/disabling the CORE namespace
+ */
+#undef VFS_NAMESPACE_BEGIN
+#undef VFS_NAMESPACE_END   
+#ifdef USE_VFS_NAMESPACE
+  #define VFS_NAMESPACE_BEGIN   namespace VFS {
+  #define VFS_NAMESPACE_END     }
+#else
+  #define VFS_NAMESPACE_BEGIN
+  #define VFS_NAMESPACE_END
+#endif /* USE_VFS_NAMESPACE ? */
+
+/*-------------------------------------------------------------------------*/
+
+/*
+ *      When compiling the gucefVFS library you should define BUILD_GUCEFVFS_DLL
+ *      before including this macro file.
+ */
+#ifndef MANUAL_EXPORT_DEFINE
+  #undef USE_DLL
+  #undef BUILD_DLL
+  #ifdef BUILD_GUCEFVFS_DLL
+    #define BUILD_DLL
+  #else
+    #define USE_DLL 
+  #endif
+#endif /* MANUAL_EXPORT_DEFINE ? */
+
+/*-------------------------------------------------------------------------*/
+
+/*
+ *      Macros for dynamic linking or static linking. Use the switches in the
+ *      config file to control the export type.
+ */
+#undef EXPORT
+#ifdef MSWIN_BUILD
+  #ifdef BUILD_DLL
+    #define EXPORT __declspec( dllexport )
+  #else
+    #ifdef USE_DLL
+      #define EXPORT __declspec( dllimport )
+    #else
+      #define EXPORT
+    #endif /* USE_DLL */
+  #endif /* BUILD_DLL */
+#else
+  #define EXPORT   /* Linux does not need an additional directive */
+#endif /* MSWIN_BUILD ? */
+
+#undef EXPORT_CPP
+#ifdef EXPORT_CPP_CODE
+  #define EXPORT_CPP EXPORT
+#else
+  #define EXPORT_CPP
+#endif /* EXPORT_CPP_CODE */
+
+#undef EXPORT_C 
+#ifdef EXPORT_C_CODE
+  #define EXPORT_C EXPORT
+#else
+  #define EXPORT_C
+#endif /* EXPORT_C_CODE */
+
+/*-------------------------------------------------------------------------*/
+
+/*
+ *      Change calling convention notation depending on the compiler used.
+ */
+#if defined ( __BORLANDC__ ) || defined ( _MSC_VER )
+  #define CALLSPEC_C __cdecl
+#else  
+  #define CALLSPEC_C cdecl
+#endif  
+#if defined ( __BORLANDC__ ) || defined ( _MSC_VER )
+  #define CALLSPEC_STD __stdcall
+#else  
+  #define CALLSPEC_STD stdcall
+#endif 
+
+#undef CALLSPEC_TYPE
+#ifdef USE_CALLSPEC_C 
+  #define CALLSPEC_TYPE CALLSPEC_C
+#else 
+  #ifdef USE_CALLSPEC_STD  
+    #define CALLSPEC_TYPE CALLSPEC_STD
+  #else  
+    #ifdef USE_CALLSPEC_PASCAL    
+      #define CALLSPEC_TYPE CALLSPEC_PASCAL
+    #else 
+       #ifdef USE_CALLSPEC_FAST  
+         #define CALLSPEC_TYPE CALLSPEC_FAST
+       #else  
+         #error No calling convention configuration was specified
+       #endif    
+     #endif
+  #endif
+#endif  
+
+#undef PLUGIN_CALLSPEC_TYPE
+#ifdef USE_PLUGIN_CALLSPEC_C 
+  #define PLUGIN_CALLSPEC_TYPE CALLSPEC_C
+#else 
+  #ifdef USE_PLUGIN_CALLSPEC_STD  
+    #define PLUGIN_CALLSPEC_TYPE CALLSPEC_STD
+  #else  
+    #ifdef USE_PLUGIN_CALLSPEC_PASCAL    
+      #define PLUGIN_CALLSPEC_TYPE CALLSPEC_PASCAL
+    #else 
+       #ifdef USE_PLUGIN_CALLSPEC_FAST  
+         #define PLUGIN_CALLSPEC_TYPE CALLSPEC_FAST
+       #else  
+         #error No calling convention configuration was specified for plugins 
+       #endif    
+     #endif
+  #endif
+#endif        
+      
+/*
+ *      Macro that switches between a calling convention prefix and
+ *      postfix notation.
+ */
+#undef CALLSPEC_PREFIX
+#undef CALLSPEC_SUFFIX
+#undef PLUGIN_CALLSPEC_PREFIX
+#undef PLUGIN_CALLSPEC_SUFFIX
+#if defined ( __BORLANDC__ ) || defined ( _MSC_VER )
+  #define CALLSPEC_PREFIX CALLSPEC_TYPE
+  #define CALLSPEC_SUFFIX
+  #define PLUGIN_CALLSPEC_PREFIX PLUGIN_CALLSPEC_TYPE  
+  #define PLUGIN_CALLSPEC_SUFFIX
+#else
+  #define CALLSPEC_PREFIX
+  #define CALLSPEC_SUFFIX  __attribute__((CALLSPEC_TYPE))
+  #define PLUGIN_CALLSPEC_PREFIX   
+  #define PLUGIN_CALLSPEC_SUFFIX __attribute__((PLUGIN_CALLSPEC_TYPE)) 
+#endif
+
+/*-------------------------------------------------------------------------*/
+
+/*
+ *      Detection of 32/64 bits builds. 
+ */
+#ifndef MANUAL_BITTARGET_DEFINE
+  #undef GUCEF32BIT
+  #undef GUCEF64BIT
+  #if defined ( MSWIN_BUILD ) && ( defined( _WIN32 ) || defined( WIN32 ) )
+    #define GUCEF32BIT
+  #elif defined ( MSWIN_BUILD ) && ( defined( _WIN64 ) || defined( WIN64 ) )
+    #define GUCEF64BIT
+  #else  
+    #error Cannot autodetect wheter this is a 32 or 64 bit build
+  #endif
+#endif
+
+/*-------------------------------------------------------------------------*/
+
+/*
+ *      Usefull macro when parsing path strings cross-platform
+ *      Allows you to use the same code to swap out dir seperators
+ */
+#undef DIRSEPCHAR
+#undef DIRSEPCHAROPPOSITE
+#ifdef MSWIN_BUILD
+  #define DIRSEPCHAR         '\\'
+  #define DIRSEPCHAROPPOSITE '/'
+#else
+  #define DIRSEPCHAR         '/'
+  #define DIRSEPCHAROPPOSITE '\\'
+#endif 
+
+/*-------------------------------------------------------------------------*/
+
+/*
+ *      Small macro that changes integers used for storing memory 
+ *      addresses to 32 or 64 bit depending on the build target.
+ *      Usage of INTPTR allows you to safely cast or manipulate a 
+ *      pointer as an integer.  
+ */
+#undef INTPTR 
+#ifdef GUCEF32BIT
+  #define INTPTR UInt32
+#else /* 64 Bit target */
+  #define INTPTR UInt64
+#endif  
+
+/*-------------------------------------------------------------------------*/
+
+#ifdef DEBUG_MODE
+  #ifndef TSPRINTING_H
+    #include "tsprinting.h"
+    #define TSPRINTING_H
+  #endif /* TSPRINTING_H ? */
+#endif /* DEBUG_MODE ? */
+
+/*-------------------------------------------------------------------------*/
+
+#undef DEBUGOUTPUT 
+#undef DEBUGOUTPUTs
+#undef DEBUGOUTPUTi
+#undef DEBUGOUTPUTss
+#undef DEBUGOUTPUTsi
+#undef DEBUGOUTPUTis
+#undef DEBUGOUTPUTsss
+#ifdef DEBUG_MODE
+  #ifdef ADD_EXTRA_INFO_TO_DEBUGOUTPUT
+    #define DEBUGOUTPUT( c_str ) ( CORE::tsprintf( "%s @ Line %d File %s\n", c_str, __LINE__, __FILE__ ) )
+    #define DEBUGOUTPUTs DEBUGOUTPUT
+    #define DEBUGOUTPUTi( intval ) ( CORE::tsprintf( "%i @ Line %d File %s\n", intval, __LINE__, __FILE__ ) )    
+    #define DEBUGOUTPUTss( c_str1, c_str2 ) ( CORE::tsprintf( "%s%s @ Line %d File %s\n", c_str1, c_str2, __LINE__, __FILE__ ) )
+    #define DEBUGOUTPUTis( intval, c_str ) ( CORE::tsprintf( "%i%s @ Line %d File %s\n", intval, c_str, __LINE__, __FILE__ ) )
+    #define DEBUGOUTPUTsi( c_str, intval ) ( CORE::tsprintf( "%s%i @ Line %d File %s\n", c_str, intval, __LINE__, __FILE__ ) )    
+    #define DEBUGOUTPUTsss( c_str1, c_str2, c_str3 ) ( CORE::tsprintf( "%s%s%s @ Line %d File %s\n", c_str1, c_str2, c_str3, __LINE__, __FILE__ ) )
+  #else
+    #define DEBUGOUTPUT( c_str ) ( CORE::tsprintf( "%s\n", c_str ) )
+    #define DEBUGOUTPUTs DEBUGOUTPUT
+    #define DEBUGOUTPUTi( intval ) ( CORE::tsprintf( "%i\n", intval ) )    
+    #define DEBUGOUTPUTss( c_str1, c_str2 ) ( CORE::tsprintf( "%s%s\n", c_str1, c_str2 ) )
+    #define DEBUGOUTPUTis( intval, c_str ) ( CORE::tsprintf( "%i%s\n", intval, c_str ) )
+    #define DEBUGOUTPUTsi( c_str, intval ) ( CORE::tsprintf( "%s%i\n", c_str, intval ) )
+    #define DEBUGOUTPUTsss( c_str1, c_str2, c_str3 ) ( CORE::tsprintf( "%s%s%s\n", c_str1, c_str2, c_str3 ) )
+  #endif  
+#else
+  #define DEBUGOUTPUT( c_str ) ( )
+  #define DEBUGOUTPUTi ( intval ) ( )
+  #define DEBUGOUTPUTs DEBUGOUTPUT
+  #define DEBUGOUTPUTss( c_str1, c_str2 ) ( )
+  #define DEBUGOUTPUTsi( c_str, intval ) ( )
+  #define DEBUGOUTPUTis( intval, c_str ) ( )
+  #define DEBUGOUTPUTsss( c_str1, c_str2, c_str3 ) ( )
+#endif /* DEBUG_MODE ? */  
+
+/*-------------------------------------------------------------------------*/
+
+#endif /* GUCEFVFS_MACROS_H ? */
+
+/*-------------------------------------------------------------------------//
+//                                                                         //
+//      Info & Changes                                                     //
+//                                                                         //
+//-------------------------------------------------------------------------//
+
+- 26-03-2005 :
+       - Initial version of this file.
+
+-----------------------------------------------------------------------------*/
