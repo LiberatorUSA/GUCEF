84fc54f321c9a57b3434d808e7e900db51b4306d
liberatorusa <liberatorusa@9712d391-321d-0410-84f4-d6a0ebf28296>
2006-09-11 20:39:09 +0000

    Added items remotely
    G:\SVNWORK\GUALL\GUCEF\gucefCOM_TestApp\projects
    G:\SVNWORK\GUALL\GUCEF\gucefCOM_TestApp\src
    G:\SVNWORK\GUALL\GUCEF\gucefCOM_TestApp\Debug_MVC7
    G:\SVNWORK\GUALL\GUCEF\gucefCOM_TestApp\include

---
+++ gucefCOM_TestApp/src/TestCode_CHTTPClient.cpp
@@ -0,0 +1,178 @@
+/*
+ * Copyright (C) Dinand Vanvelzen. 2002 - 2005.  All rights reserved.
+ *
+ * All source code herein is the property of Dinand Vanvelzen. You may not sell
+ * or otherwise commercially exploit the source or things you created based on
+ * the source.
+ *
+ * THE SOFTWARE IS PROVIDED "AS-IS" AND WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY
+ * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
+ * IN NO EVENT SHALL DINAND VANVELZEN BE LIABLE FOR ANY SPECIAL, INCIDENTAL,
+ * INDIRECT OR CONSEQUENTIAL DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER
+ * RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER OR NOT ADVISED OF
+ * THE POSSIBILITY OF DAMAGE, AND ON ANY THEORY OF LIABILITY, ARISING OUT
+ * OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+ 
+/*-------------------------------------------------------------------------//
+//                                                                         //
+//      INCLUDES                                                           //
+//                                                                         //
+//-------------------------------------------------------------------------*/
+
+#ifndef GUCEFCORE_H
+#include "gucefCORE.h"
+#define GUCEFCORE_H
+#endif /* GUCEFCORE_H ? */
+
+#ifndef GUCEFCOMCORE_H
+#include "gucefCOMCORE.h"
+#define GUCEFCOMCORE_H
+#endif /* GUCEFCOMCORE_H ? */
+
+#ifndef GUCEFCOM_H
+#include "gucefCOM.h"
+#define GUCEFCOM_H
+#endif /* GUCEFCOM_H ? */
+
+#include "TestCode_CHTTPClient.h"
+
+/*-------------------------------------------------------------------------//
+//                                                                         //
+//      UTILITIES                                                          //
+//                                                                         //
+//-------------------------------------------------------------------------*/ 
+
+using namespace GUCEF;
+using namespace GUCEF::CORE;
+using namespace GUCEF::COMCORE;
+using namespace GUCEF::COM;
+
+/*-------------------------------------------------------------------------*/
+
+class CMyHTTPClient : public CHTTPClientInterface
+{        
+        public:
+        
+        CMyHTTPClient( void )
+        {
+                m_client.SetInterface( this );        
+        }
+        
+        protected:
+        
+        virtual void OnConnected( CHTTPClient& client )
+        {
+                printf( "CMyHTTPClient::OnConnected()\n" );
+        }
+        
+        virtual void OnDisconnected( CHTTPClient& client )
+        {
+                printf( "CMyHTTPClient::OnDisconnected()\n" );
+        }
+        
+        virtual void OnHTTPError( CHTTPClient& client ,
+                                  THTTPCODE errorcode )
+        {
+                printf( "CMyHTTPClient::OnHTTPError()\n" );
+        }                                  
+        
+        virtual void OnError( CHTTPClient& client )
+        {
+                printf( "CMyHTTPClient::OnError()\n" );
+        }
+        
+        virtual void OnRedirect( CHTTPClient& client                  ,
+                                 const GUCEF::CORE::CString& location )
+        {
+                printf( "CMyHTTPClient::OnRedirect()\n" );
+                printf( "CMyHTTPClient: location: %s\n", location.C_String() );
+        }
+        
+        virtual void OnDataRecieved( CHTTPClient& client ,
+                                     const char *data    ,
+                                     CORE::UInt32 length       )
+        {
+                printf( "CMyHTTPClient::OnDataRead()\n" );
+                printf( "CMyHTTPClient: recieved %d bytes\n", length );
+        }
+        
+        virtual void OnDataSend( CHTTPClient& client ,
+                                 const char *data    ,
+                                 CORE::UInt32 length       )
+        {
+                printf( "CMyHTTPClient::OnDataSend()\n" );
+                printf( "CMyHTTPClient: sending %d bytes\n", length );
+        }
+        
+        virtual void OnContent( CHTTPClient& client      ,
+                                const CORE::UInt32 size        ,
+                                const bool resumeable    ,
+                                const THTTPCODE httpcode )
+        {
+                printf( "CMyHTTPClient::OnContent()\n" );
+                printf( "CMyHTTPClient: size: %d\n", size );        
+                printf( "CMyHTTPClient: resumeable: %d\n", resumeable );
+                printf( "CMyHTTPClient: httpcode: %d\n", httpcode );
+        }                                   
+                                 
+        virtual void OnFinish( CHTTPClient &client )
+        {
+                printf( "CMyHTTPClient::OnFinish()\n" );
+        }                                              
+
+        public:
+        CHTTPClient m_client;
+};
+
+/*-------------------------------------------------------------------------*/
+
+class CMyPumpClient : public CEventPumpClient
+{
+        public:
+        
+        CMyPumpClient( void )
+        {
+                m_startevent = GUCEF::CORE::CEventTypeRegistry::Instance()->GetType( "GUCEF::CORE::CGUCEFApplication::INIT" );       
+        }
+        
+        protected:
+        
+        virtual void OnUpdate( const CORE::UInt32 tickcount  ,
+                               const CORE::UInt32 deltaticks )
+        {
+        }
+        
+        virtual void OnProcessEvent( const CEvent& event )
+        {
+                if ( event.GetEventType() == m_startevent )
+                {
+                        if ( client.m_client.Get( "www.google.com" ,
+                                                  80               ,
+                                                  ""               ) )
+                        {
+                                printf( "Get succeeded\n" );
+                        }
+                        else                        
+                        {
+                                printf( "Get failed\n" );
+                        }                                                  
+                }                                                 
+        }
+        
+        private:
+        CMyHTTPClient client;
+        CORE::UInt32 m_startevent;        
+                                              
+};
+
+/*-------------------------------------------------------------------------*/
+
+void
+SetupHTTPClientTest( void )
+{
+        CMyPumpClient* pclient = new CMyPumpClient();
+}
+
+/*-------------------------------------------------------------------------*/
---
+++ gucefCOM_TestApp/src/gucefCOM_TestApp_main.cpp
@@ -0,0 +1,90 @@
+/*
+ * Copyright (C) Dinand Vanvelzen. 2002 - 2005.  All rights reserved.
+ *
+ * All source code herein is the property of Dinand Vanvelzen. You may not sell
+ * or otherwise commercially exploit the source or things you created based on
+ * the source.
+ *
+ * THE SOFTWARE IS PROVIDED "AS-IS" AND WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY 
+ * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
+ * IN NO EVENT SHALL DINAND VANVELZEN BE LIABLE FOR ANY SPECIAL, INCIDENTAL, 
+ * INDIRECT OR CONSEQUENTIAL DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER 
+ * RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER OR NOT ADVISED OF 
+ * THE POSSIBILITY OF DAMAGE, AND ON ANY THEORY OF LIABILITY, ARISING OUT 
+ * OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE. 
+ */
+
+/*-------------------------------------------------------------------------//
+//                                                                         //
+//      INCLUDES                                                           //
+//                                                                         //
+//-------------------------------------------------------------------------*/
+
+#include <windows.h>
+
+#ifndef GUCEFCORE_H
+#include "gucefCORE.h"
+#define GUCEFCORE_H
+#endif /* GUCEFCORE_H ? */
+
+#ifndef GUCEFCOMCORE_H
+#include "gucefCOMCORE.h"
+#define GUCEFCOMCORE_H
+#endif /* GUCEFCOMCORE_H ? */
+
+#ifndef GUCEFCOM_H
+#include "gucefCOM.h"
+#define GUCEFCOM_H
+#endif /* GUCEFCOM_H ? */
+
+#include "TestCode_CHTTPClient.h"
+
+/*-------------------------------------------------------------------------//
+//                                                                         //
+//      UTILITIES                                                          //
+//                                                                         //
+//-------------------------------------------------------------------------*/
+
+
+          
+/*
+ *      Application entry point
+ */
+int __stdcall
+WinMain( HINSTANCE hinstance     ,
+         HINSTANCE hprevinstance ,
+         LPSTR lpcmdline         ,
+         int ncmdshow            )
+{
+        using namespace GUCEF::CORE;
+        using namespace GUCEF::COMCORE;
+        using namespace GUCEF::COM;
+                
+        try 
+        {                               
+                SetupHTTPClientTest();
+
+                CGUCEFApplication::Instance()->Main( hinstance     ,
+                                                     hprevinstance ,
+                                                     lpcmdline     ,
+                                                     ncmdshow      ,
+                                                     true          );
+                
+                return 1;                                                                            
+        }
+        catch ( ... )
+        {
+                #ifdef DEBUG_MODE
+                GUCEF_PrintCallstack();
+                GUCEF_DumpCallstack( "GUCEFCallstack.txt" );
+                #endif
+                
+                ShowErrorMessage( "Unknown exception"                                                                 ,
+                                  "Unhandled exception during program execution, the application will now terminate"  );                                                         
+        }
+        GUCEF_END;
+        return 1;                                                                                                                              
+}
+
+/*-------------------------------------------------------------------------*/
