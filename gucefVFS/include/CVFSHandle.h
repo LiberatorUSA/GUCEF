64fad943ef6da380f8876ff15c152a4de09a2891
liberatorusa <liberatorusa@9712d391-321d-0410-84f4-d6a0ebf28296>
2006-09-11 19:52:00 +0000

    Added items remotely
    G:\SVNWORK\GUALL\GUCEF\gucefVFS\include\gucefVFSDLLInit.h
    G:\SVNWORK\GUALL\GUCEF\gucefVFS\include\CFilePackage.h
    G:\SVNWORK\GUALL\GUCEF\gucefVFS\include\CFPCodec.h
    G:\SVNWORK\GUALL\GUCEF\gucefVFS\include\CFPCodecPlugin.h
    G:\SVNWORK\GUALL\GUCEF\gucefVFS\include\CFPCodecPluginManager.h
    G:\SVNWORK\GUALL\GUCEF\gucefVFS\include\CFPCodecPluginRef.h
    G:\SVNWORK\GUALL\GUCEF\gucefVFS\include\CFPCodecRegistry.h
    G:\SVNWORK\GUALL\GUCEF\gucefVFS\include\CGUCEFVFSModule.h
    G:\SVNWORK\GUALL\GUCEF\gucefVFS\include\CVFS.H
    G:\SVNWORK\GUALL\GUCEF\gucefVFS\include\CVFSHandle.h
    G:\SVNWORK\GUALL\GUCEF\gucefVFS\include\CVFSURLHandler.h
    G:\SVNWORK\GUALL\GUCEF\gucefVFS\include\CVFSURLResource.h
    G:\SVNWORK\GUALL\GUCEF\gucefVFS\include\gucefVFS.h
    G:\SVNWORK\GUALL\GUCEF\gucefVFS\include\gucefVFS_config.h
    G:\SVNWORK\GUALL\GUCEF\gucefVFS\include\gucefVFS_ETypes.h
    G:\SVNWORK\GUALL\GUCEF\gucefVFS\include\gucefVFS_macros.h

---
+++ gucefVFS/include/CVFSHandle.h
@@ -0,0 +1,116 @@
+/*
+ * Copyright (C) Dinand Vanvelzen. 2002 - 2005.  All rights reserved.
+ *
+ * All source code herein is the property of Dinand Vanvelzen. You may not sell
+ * or otherwise commercially exploit the source or things you created based on
+ * the source.
+ *
+ * THE SOFTWARE IS PROVIDED "AS-IS" AND WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS, IMPLIED OR OTHERWISE, INCLUDING WITHOUT LIMITATION, ANY 
+ * WARRANTY OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE.
+ * IN NO EVENT SHALL DINAND VANVELZEN BE LIABLE FOR ANY SPECIAL, INCIDENTAL, 
+ * INDIRECT OR CONSEQUENTIAL DAMAGES OF ANY KIND, OR ANY DAMAGES WHATSOEVER 
+ * RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER OR NOT ADVISED OF 
+ * THE POSSIBILITY OF DAMAGE, AND ON ANY THEORY OF LIABILITY, ARISING OUT 
+ * OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE. 
+ */
+
+#ifndef CVFSHANDLE_H
+#define CVFSHANDLE_H
+
+/*-------------------------------------------------------------------------//
+//                                                                         //
+//      INCLUDES                                                           //
+//                                                                         //
+//-------------------------------------------------------------------------*/
+
+#ifndef GUCEF_CORE_CSTRING_H
+#include "CDVString.h"
+#define GUCEF_CORE_CSTRING_H
+#endif /* GUCEF_CORE_CSTRING_H ? */
+
+#ifndef GUCEF_CORE_CIOACCESS_H
+#include "CIOAccess.h"
+#define GUCEF_CORE_CIOACCESS_H
+#endif /* GUCEF_CORE_CIOACCESS_H ? */
+
+#ifndef GUCEF_CORE_CDYNAMICARRAY_H
+#include "CDynamicArray.h"
+#define GUCEF_CORE_CDYNAMICARRAY_H
+#endif /* GUCEF_CORE_CDYNAMICARRAY_H ? */
+
+#ifndef GUCEFVFS_MACROS_H
+#include "gucefVFS_macros.h"         /* often used gucefVFS macros */
+#define GUCEFVFS_MACROS_H
+#endif /* GUCEFVFS_MACROS_H ? */
+
+/*-------------------------------------------------------------------------//
+//                                                                         //
+//      NAMESPACE                                                          //
+//                                                                         //
+//-------------------------------------------------------------------------*/
+
+GUCEF_NAMESPACE_BEGIN
+VFS_NAMESPACE_BEGIN
+
+/*-------------------------------------------------------------------------//
+//                                                                         //
+//      CLASSES                                                            //
+//                                                                         //
+//-------------------------------------------------------------------------*/
+
+class EXPORT_CPP CVFSHandle
+{
+        public:
+        
+        CORE::CIOAccess* GetAccess( void ) const;
+        
+        const CORE::CString& GetFilename( void ) const;
+        
+        bool IsPacked( void ) const;      
+        
+        private:
+        friend class CVFS;
+        friend class CFilePackage;
+        
+        CVFSHandle( CORE::CIOAccess* fileaccess   ,
+                    const CORE::CString& filename ,
+                    bool packed                   ,
+                    UInt32 index                  );
+                    
+        ~CVFSHandle();
+        
+        private:
+        CVFSHandle( void );
+        CVFSHandle( const CVFSHandle& src );
+        CVFSHandle& operator=( const CVFSHandle& src );
+        
+        CORE::CIOAccess* _fileaccess;
+        CORE::CString _filename;
+        bool _packed;
+        UInt32 _index;
+};
+
+/*-------------------------------------------------------------------------//
+//                                                                         //
+//      NAMESPACE                                                          //
+//                                                                         //
+//-------------------------------------------------------------------------*/
+
+VFS_NAMESPACE_END
+GUCEF_NAMESPACE_END
+
+/*-------------------------------------------------------------------------*/
+          
+#endif /* CGUCEFVFSMODULE_H ? */
+
+/*-------------------------------------------------------------------------//
+//                                                                         //
+//      Info & Changes                                                     //
+//                                                                         //
+//-------------------------------------------------------------------------//
+
+- 12-02-2005 :
+        - Initial implementation
+
+-----------------------------------------------------------------------------*/
