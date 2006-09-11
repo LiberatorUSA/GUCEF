cceace98daf13cc7b2474ae3832edf3972f344d6
liberatorusa <liberatorusa@9712d391-321d-0410-84f4-d6a0ebf28296>
2006-09-11 20:58:20 +0000

    Added items remotely
    G:\SVNWORK\GUALL\GUCEF\PatcherGUI\projects
    G:\SVNWORK\GUALL\GUCEF\PatcherGUI\src
    G:\SVNWORK\GUALL\GUCEF\PatcherGUI\include

---
+++ tools/MFCPatcherGUI/projects/MVC8/resource.h
@@ -0,0 +1,20 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by PatcherGUI.rc
+//
+#define IDD_PATCHERGUI_DIALOG           102
+#define IDR_MAINFRAME                   128
+#define IDC_LIST1                       1000
+#define IDC_PROGRESS1                   1001
+#define IDC_PROGRESS2                   1002
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1003
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
