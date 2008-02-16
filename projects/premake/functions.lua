e334a63ce97c6ece402edcc3df905955072474ee
liberatorusa <liberatorusa@9712d391-321d-0410-84f4-d6a0ebf28296>
2008-02-16 08:05:54 +0000

    tweaking premake

--- projects/premake/functions.lua
+++ projects/premake/functions.lua
@@ -1,3 +1,10 @@
+
+--[[---------------------------------------------------------------------------]]
+--[[                                                                           ]]
+--[[    Main build script setup function                                       ]]
+--[[                                                                           ]]
+--[[---------------------------------------------------------------------------]]
+
 function project_setup(project_name)
     addoption("compiler", "Specify compiler type (eg. MVC8, MVC7.1, GCC)")
     
@@ -28,14 +35,19 @@ function project_setup(project_name)
     os.execute("mkdir \"..\\..\\"..common["Bin/Release"].."\"")
 end
 
-function add_package(package_name)
+--[[---------------------------------------------------------------------------]]
+--[[                                                                           ]]
+--[[    Adds a module to the build script                                      ]]
+--[[                                                                           ]]
+--[[---------------------------------------------------------------------------]]
+
+function add_package( package_name )
     package = newpackage()
 
     package.name = package_name.."_"..compiler
     package.path = "../../"..package_name.."/projects/"..compiler
-    package.kind = "dll"
     package.language = "c"
-
+    package.kind = "dll"
     package.files = {
         matchrecursive("../../include/*.h", "../../include/mswin/*.h", "../../src/*.c", "../../src/*.cpp", "../../mswin/src/*.cpp", "../../dependancy/*.c", "../../dependancy/*.cpp", "../../dependancy/*.h")
     }
@@ -68,11 +80,105 @@ function add_package(package_name)
     end
 end
 
-function add_package_dependancy(package_name)
+--[[---------------------------------------------------------------------------]]
+--[[                                                                           ]]
+--[[    Adds an application to the build script                                ]]
+--[[                                                                           ]]
+--[[---------------------------------------------------------------------------]]
+
+function add_application( package_name )
+    package = newpackage()
+
+    package.name = package_name.."_"..compiler
+    package.path = "../../"..package_name.."/projects/"..compiler
+    package.language = "c"
+    package.kind = "winexe"
+    package.files = {
+        matchrecursive("../../include/*.h", "../../include/mswin/*.h", "../../src/*.c", "../../src/*.cpp", "../../mswin/src/*.cpp", "../../dependancy/*.c", "../../dependancy/*.cpp", "../../dependancy/*.h")
+    }
+
+    package.includepaths = { "../../include", "../../include/mswin", "../../dependancy", "../../../common/include" }
+
+    package.config["Debug"].bindir = "../../../"..package_name.."/bin/Debug_"..compiler
+    package.config["Debug"].libdir = "../../../"..package_name.."/bin/Debug_"..compiler
+    package.config["Debug"].objdir = "../../../"..package_name.."/bin/Debug_"..compiler
+
+    package.config["Debug"].defines = { "DEBUG", "_DEBUG" }
+    package.config["Debug"].target = package_name.."_"..compiler.."_d"
+    
+    package.config["Debug"].postbuildcommands = { "copy &quot;..\\..\\..\\"..package_name.."\\bin\\Debug_"..compiler.."\\"..package.config["Debug"].target..".dll&quot; &quot;..\\..\\..\\"..common["Bin/Debug"].."&quot;" }
+    
+    package.config["Release"].bindir = "../../../"..package_name.."/bin/Release_"..compiler
+    package.config["Release"].libdir = "../../../"..package_name.."/bin/Release_"..compiler
+    package.config["Release"].objdir = "../../../"..package_name.."/bin/Release_"..compiler
+
+    package.config["Release"].target = package_name.."_"..compiler
+
+    package.config["Release"].postbuildcommands = { "copy &quot;..\\..\\..\\"..package_name.."\\bin\\Release_"..compiler.."\\"..package.config["Release"].target..".dll&quot; &quot;..\\..\\..\\"..common["Bin/Debug"].."&quot;" }
+
+    for k,v in pairs(global_defines) do table.insert(package.defines, v) end
+
+    table.insert(package.defines, string.upper(package_name).."_BUILD_MODULE")
+
+    if(compiler == "MVC8") then
+        table.insert(package.defines, string.upper(package_name).."_VC8_EXPORTS")
+    end
+end
+
+--[[---------------------------------------------------------------------------]]
+--[[                                                                           ]]
+--[[    Adds a project dependency to the project                               ]]
+--[[                                                                           ]]
+--[[---------------------------------------------------------------------------]]
+
+function add_package_dependency(package_name)
     table.insert(package.includepaths, 3, "../../../"..package_name.."/include")
     table.insert(package.links, package_name.."_"..compiler)
 end
 
-function add_library_dependancy(lib_name)
+--[[---------------------------------------------------------------------------]]
+--[[                                                                           ]]
+--[[    Adds a library dependency to the project                               ]]
+--[[                                                                           ]]
+--[[---------------------------------------------------------------------------]]
+
+function add_library_dependency(lib_name)
     table.insert(package.links, lib_name)
+end
+
+--[[---------------------------------------------------------------------------]]
+--[[                                                                           ]]
+--[[    Specialization for the windows platform, no-op on other platforms      ]]
+--[[                                                                           ]]
+--[[---------------------------------------------------------------------------]]
+
+function add_windows_library_dependency( lib_name )
+    if ( windows ) then
+        add_library_dependency( lib_name )
+    end
+end
+
+--[[---------------------------------------------------------------------------]]
+--[[                                                                           ]]
+--[[    Function to add packages defined in a file                             ]]
+--[[                                                                           ]]
+--[[---------------------------------------------------------------------------]]
+
+function add_packages_from_file( file_path )
+    original_cwd = os.getcwd()
+    os.chdir( file_path )
+    dopackage( file_path )
+    os.chdir( original_cwd )
+end
+
+--[[---------------------------------------------------------------------------]]
+--[[                                                                           ]]
+--[[    Function to add packages defined in a file passed as an option         ]]
+--[[                                                                           ]]
+--[[---------------------------------------------------------------------------]]
+
+function add_packages_from_file_option( option_value )
+    if ( options[ option_value ] ) then
+        add_packages_from_file( options[ option_value ] )
+    end
 end
