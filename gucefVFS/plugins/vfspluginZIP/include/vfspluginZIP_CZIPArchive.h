5362a27a45aed415514fb378d1e7e4bd78314e6f
liberatorusa <liberatorusa@9712d391-321d-0410-84f4-d6a0ebf28296>
2008-08-02 11:10:37 +0000

    Initial version of ZIP Archive support

---
+++ gucefVFS/plugins/vfspluginZIP/include/vfspluginZIP_CZIPArchive.h
@@ -0,0 +1,160 @@
+/*
+ *  vfspluginZIP: Generic GUCEF VFS plugin for ZIP archives
+ *  Copyright (C) 2002 - 2008.  Dinand Vanvelzen
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
+ */
+
+#ifndef VFSPLUGIN_ZIP_CZIPARCHIVE_H
+#define VFSPLUGIN_ZIP_CZIPARCHIVE_H
+
+/*-------------------------------------------------------------------------//
+//                                                                         //
+//      INCLUDES                                                           //
+//                                                                         //
+//-------------------------------------------------------------------------*/
+
+#include <map>
+
+#ifndef GUCEF_VFS_CVFS_H
+#include "CVFS.h"
+#define GUCEF_VFS_CVFS_H
+#endif /* GUCEF_VFS_CVFS_H ? */
+
+#ifndef GUCEF_VFSPLUGIN_ZIP_MACROS_H
+#include "vfspluginZIP_macros.h"
+#define GUCEF_VFSPLUGIN_ZIP_MACROS_H
+#endif /* GUCEF_VFSPLUGIN_ZIP_MACROS_H ? */
+
+/*-------------------------------------------------------------------------//
+//                                                                         //
+//      TYPES                                                              //
+//                                                                         //
+//-------------------------------------------------------------------------*/
+
+// Forward declaration for zziplib to avoid header file dependency.
+typedef struct zzip_dir		ZZIP_DIR;
+typedef struct zzip_file	ZZIP_FILE;
+
+/*-------------------------------------------------------------------------//
+//                                                                         //
+//      NAMESPACE                                                          //
+//                                                                         //
+//-------------------------------------------------------------------------*/
+
+namespace GUCEF {
+namespace VFSPLUGIN {
+namespace ZIP {
+
+/*-------------------------------------------------------------------------//
+//                                                                         //
+//      CLASSES                                                            //
+//                                                                         //
+//-------------------------------------------------------------------------*/
+
+class CZIPArchive : public VFS::CIArchive
+{
+    public:
+    
+    CZIPArchive( void );
+    
+    virtual ~CZIPArchive();
+    
+    virtual CVFSHandlePtr GetFile( const VFS::CString& file          ,
+                                   const char* mode = "rb"           ,
+                                   const VFS::UInt32 memLoadSize = 0 ,
+                                   const bool overwrite = false      );
+                                  
+    virtual void GetList( TStringSet& outputList             ,
+                          const VFS::CString& location       , 
+                          bool recursive = false             ,
+                          bool includePathInFilename = false ,
+                          const VFS::CString& filter = ""    ,
+                          bool addFiles = true               ,
+                          bool addDirs  = false              ) const;
+    
+    virtual bool FileExists( const VFS::CString& filePath ) const;
+    
+    virtual VFS::UInt32 GetFileSize( const VFS::CString& filePath ) const;
+    
+    virtual VFS::CString GetFileHash( const VFS::CString& file ) const;
+    
+    virtual const VFS::CString& GetArchiveName( void ) const;
+    
+    virtual bool IsWriteable( void ) const;
+    
+    virtual bool LoadArchive( const VFS::CString& archiveName ,
+                              const VFS::CString& archivePath ,
+                              const bool writableRequest );
+                              
+    virtual bool UnloadArchive( void );    
+
+    virtual void DestroyObject( VFS::CVFSHandle* objectToBeDestroyed );
+
+    private:
+
+    struct SZipEntry 
+    {
+        VFS::CString path;
+        size_t compressedSize;
+        size_t uncompressedSize;
+        bool isDir;
+    };
+    typedef struct SZipEntry TZipEntry;
+    
+    CZIPArchive( const CZIPArchive& src );
+    CZIPArchive& operator=( const CZIPArchive& src );
+
+    void CheckZzipError( int zzipError                 ,
+                         const VFS::CString& operation ) const;
+
+    CORE::CIOAccess*
+    LoadFile( const VFS::CString& file      ,
+              const VFS::UInt32 memLoadSize ) const;
+    
+    private:
+    
+    typedef std::vector< TZipEntry > TZipEntryVector;
+    
+    VFS::CString m_archiveName;
+    VFS::CString m_archivePath;
+    ZZIP_DIR* m_zipRoot;
+    TZipEntryVector m_fileList; 
+};
+
+/*-------------------------------------------------------------------------//
+//                                                                         //
+//      NAMESPACE                                                          //
+//                                                                         //
+//-------------------------------------------------------------------------*/
+
+}; /* namespace ZIP */
+}; /* namespace VFSPLUGIN */
+}; /* namespace GUCEF */
+
+/*-------------------------------------------------------------------------*/
+
+#endif /* VFSPLUGIN_ZIP_CZIPARCHIVE_H ? */
+
+/*-------------------------------------------------------------------------//
+//                                                                         //
+//      Info & Changes                                                     //
+//                                                                         //
+//-------------------------------------------------------------------------//
+
+- 04-05-2005 :
+        - Dinand: Initial version.
+
+-----------------------------------------------------------------------------*/
---
+++ gucefVFS/plugins/vfspluginZIP/src/vfspluginZIP_CZipIOAccess.cpp
@@ -0,0 +1,223 @@
+/*
+ *  vfspluginZIP: Generic GUCEF VFS plugin for ZIP archives
+ *  Copyright (C) 2002 - 2008.  Dinand Vanvelzen
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
+ */
+
+/*-------------------------------------------------------------------------//
+//                                                                         //
+//      INCLUDES                                                           //
+//                                                                         //
+//-------------------------------------------------------------------------*/
+
+#include "zzip.h"
+
+#include "vfspluginZIP_CZipIOAccess.h"
+
+/*-------------------------------------------------------------------------//
+//                                                                         //
+//      NAMESPACE                                                          //
+//                                                                         //
+//-------------------------------------------------------------------------*/
+
+namespace GUCEF {
+namespace VFSPLUGIN {
+namespace ZIP {
+
+/*-------------------------------------------------------------------------//
+//                                                                         //
+//      UTILITIES                                                          //
+//                                                                         //
+//-------------------------------------------------------------------------*/
+
+CZipIOAccess::CZipIOAccess( ZZIP_FILE* zzipFile     , 
+                            size_t uncompressedSize )
+    : CORE::CIOAccess()          ,
+      m_zzipFile( zzipFile )     ,
+      m_size( uncompressedSize )
+{GUCEF_TRACE;
+
+}
+
+/*-------------------------------------------------------------------------*/
+
+CZipIOAccess::~CZipIOAccess()
+{GUCEF_TRACE;
+
+    Close();
+}
+
+/*-------------------------------------------------------------------------*/
+
+void
+CZipIOAccess::Open( void )
+{GUCEF_TRACE;
+
+    // N/A
+}
+
+/*-------------------------------------------------------------------------*/
+
+void
+CZipIOAccess::Close( void )
+{GUCEF_TRACE;
+
+    zzip_file_close( m_zzipFile );
+    m_zzipFile = NULL;
+}
+
+/*-------------------------------------------------------------------------*/
+
+bool
+CZipIOAccess::Opened( void ) const
+{GUCEF_TRACE;
+
+    return NULL != m_zzipFile;
+}
+
+/*-------------------------------------------------------------------------*/
+
+VFS::CString
+CZipIOAccess::ReadLine( void )
+{GUCEF_TRACE;
+    
+    return VFS::CString();
+}
+
+/*-------------------------------------------------------------------------*/
+
+VFS::CString
+CZipIOAccess::ReadString( void )
+{GUCEF_TRACE;
+
+    return VFS::CString();
+}
+
+/*-------------------------------------------------------------------------*/
+
+CORE::UInt32
+CZipIOAccess::Read( void *dest            ,
+                    CORE::UInt32 esize    ,
+                    CORE::UInt32 elements )
+{GUCEF_TRACE;
+
+    return (CORE::UInt32) zzip_file_read( m_zzipFile, (char*)dest, esize*elements );
+}
+
+/*-------------------------------------------------------------------------*/
+
+CORE::UInt32
+CZipIOAccess::Tell( void ) const
+{GUCEF_TRACE;
+
+    return (CORE::UInt32) zzip_tell( m_zzipFile );
+}
+
+/*-------------------------------------------------------------------------*/
+
+CORE::Int32
+CZipIOAccess::Seek( CORE::Int32 offset ,
+                    CORE::Int32 origin )
+{GUCEF_TRACE;
+
+    return zzip_seek( m_zzipFile, static_cast<zzip_off_t>( offset ), origin );
+}
+
+/*-------------------------------------------------------------------------*/
+
+char
+CZipIOAccess::GetChar( void )
+{GUCEF_TRACE;
+
+    char tmp = 0;
+    Read( &tmp, 1, 1 );
+    return tmp;
+}
+
+/*-------------------------------------------------------------------------*/
+
+bool
+CZipIOAccess::Eof( void ) const
+{GUCEF_TRACE;
+
+    return (zzip_tell( m_zzipFile ) >= static_cast<zzip_off_t>( m_size ) );
+}
+
+/*-------------------------------------------------------------------------*/
+    
+bool
+CZipIOAccess::IsReadable( void ) const
+{GUCEF_TRACE;
+    
+    return true;
+}
+
+/*-------------------------------------------------------------------------*/
+    
+bool
+CZipIOAccess::IsWriteable( void ) const
+{GUCEF_TRACE;
+
+    return false;
+}
+    
+/*-------------------------------------------------------------------------*/
+
+bool
+CZipIOAccess::IsValid( void )
+{GUCEF_TRACE;
+
+    return Opened();
+}
+
+/*-------------------------------------------------------------------------*/
+    
+CORE::UInt32
+CZipIOAccess::GetSize( void ) const
+{GUCEF_TRACE;
+
+    return (CORE::UInt32) m_size;
+}
+
+/*-------------------------------------------------------------------------*/
+    
+void
+CZipIOAccess::Flush( void )
+{GUCEF_TRACE;
+
+    // N/A
+}    
+
+/*-------------------------------------------------------------------------*/
+
+CORE::CICloneable*
+CZipIOAccess::Clone( void ) const
+{GUCEF_TRACE;
+
+    return NULL;
+}
+    
+/*-------------------------------------------------------------------------//
+//                                                                         //
+//      NAMESPACE                                                          //
+//                                                                         //
+//-------------------------------------------------------------------------*/
+
+}; /* namespace ZIP */
+}; /* namespace VFSPLUGIN */
+}; /* namespace GUCEF */
+
+/*-------------------------------------------------------------------------*/
