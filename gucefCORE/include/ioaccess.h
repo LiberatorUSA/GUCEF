49ed47ff7d595e63fd47570753194f2569c14411
liberatorusa <liberatorusa@9712d391-321d-0410-84f4-d6a0ebf28296>
2009-05-13 17:24:38 +0000

    - Fixed some build problems in legacy projects - Gave the *lib projects a separate output dir to avoid conflicts with multi treaded compiling

--- gucefCORE/include/ioaccess.h
+++ gucefCORE/include/ioaccess.h
@@ -37,10 +37,10 @@
 //                                                                         //
 //-------------------------------------------------------------------------*/
 
-//#ifndef GUCEF_CORE_MACROS_H
+#ifndef GUCEF_CORE_MACROS_H
 #include "gucefCORE_macros.h"  /* often used gucef macros */
-//#define GUCEF_CORE_MACROS_H
-//#endif /* GUCEF_CORE_MACROS_H ? */
+#define GUCEF_CORE_MACROS_H
+#endif /* GUCEF_CORE_MACROS_H ? */
 
 /*-------------------------------------------------------------------------//
 //                                                                         //
