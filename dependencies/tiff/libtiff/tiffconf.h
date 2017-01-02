e89c37c52f752f64c8f0f2b17174188a696fb846
LiberatorUSA <dinand@vanvelzen.us>
2017-01-02 01:01:01 -0600

    libtiff related tweaks to get it to build with the build system

--- dependencies/tiff/libtiff/tiffconf.h
+++ dependencies/tiff/libtiff/tiffconf.h
@@ -7,110 +7,153 @@
 #ifndef _TIFFCONF_
 #define _TIFFCONF_
 
-/* Signed 16-bit type */
-#undef TIFF_INT16_T
+/* The size of a `int', as computed by sizeof. */
+#define SIZEOF_INT 4
 
-/* Signed 32-bit type */
-#undef TIFF_INT32_T
-
-/* Signed 64-bit type */
-#undef TIFF_INT64_T
+/* The size of a `long', as computed by sizeof. */
+#include <limits.h>
+#if (LONG_MAX == +9223372036854775807L)
+#define SIZEOF_LONG 8
+#define SIZEOF_UNSIGNED_LONG 8
+#elif (LONG_MAX == +2147483647)
+#define SIZEOF_LONG 4
+#define SIZEOF_UNSIGNED_LONG 4
+#else
+#error "Cannot detect SIZEOF_LONG"
+#endif
 
 /* Signed 8-bit type */
-#undef TIFF_INT8_T
-
-/* Unsigned 16-bit type */
-#undef TIFF_UINT16_T
-
-/* Unsigned 32-bit type */
-#undef TIFF_UINT32_T
-
-/* Unsigned 64-bit type */
-#undef TIFF_UINT64_T
+#define TIFF_INT8_T signed char
 
 /* Unsigned 8-bit type */
-#undef TIFF_UINT8_T
+#define TIFF_UINT8_T unsigned char
 
-/* Signed size type */
-#undef TIFF_SSIZE_T
+/* Signed 16-bit type */
+#define TIFF_INT16_T signed short
+
+/* Unsigned 16-bit type */
+#define TIFF_UINT16_T unsigned short
+
+/* Signed 32-bit type */
+#define TIFF_INT32_T signed int
+
+/* Unsigned 32-bit type */
+#define TIFF_UINT32_T unsigned int
+
+/* Signed / Unsigned 64-bit type */
+#ifdef _MSC_VER
+#define TIFF_INT64_T signed __int64
+#define TIFF_UINT64_T unsigned __int64
+#else
+#include <inttypes.h>
+#define TIFF_INT64_T int64_t
+#define TIFF_UINT64_T uint64_t
+#endif // _MSC_VER
+
+/* Signed 64-bit type */
+#if defined(_WIN64)
+#define TIFF_SSIZE_T signed __int64
+#else
+#define TIFF_SSIZE_T signed long
+#endif
 
 /* Pointer difference type */
-#undef TIFF_PTRDIFF_T
-
-/* Define to 1 if the system has the type `int16'. */
-#undef HAVE_INT16
-
-/* Define to 1 if the system has the type `int32'. */
-#undef HAVE_INT32
-
-/* Define to 1 if the system has the type `int8'. */
-#undef HAVE_INT8
+#define TIFF_PTRDIFF_T ptrdiff_t
 
 /* Compatibility stuff. */
 
 /* Define as 0 or 1 according to the floating point format suported by the
    machine */
-#undef HAVE_IEEEFP
+#define HAVE_IEEEFP 1
 
+/* 
+-----------------------------------------------------------------------
+Byte order
+-----------------------------------------------------------------------
+*/
+
+/*
+Define WORDS_BIGENDIAN to 1 if your processor stores words with the most
+significant byte first (like Motorola and SPARC, unlike Intel).
+Some versions of gcc may have BYTE_ORDER or __BYTE_ORDER defined
+If your big endian system isn't being detected, add an OS specific check
+*/
+#if (defined(BYTE_ORDER) && BYTE_ORDER==BIG_ENDIAN) || \
+	(defined(__BYTE_ORDER) && __BYTE_ORDER==__BIG_ENDIAN) || \
+	defined(__BIG_ENDIAN__)
 /* Set the native cpu bit order (FILLORDER_LSB2MSB or FILLORDER_MSB2LSB) */
-#undef HOST_FILLORDER
-
-/* Native cpu byte order: 1 if big-endian (Motorola) or 0 if little-endian
-   (Intel) */
+#define HOST_FILLORDER FILLORDER_MSB2LSB
+/* Native cpu byte order: 1 if big-endian (Motorola) or 0 if little-endian (Intel) */
+#define WORDS_BIGENDIAN 1
+/* Native cpu byte order: 1 if big-endian (Motorola) or 0 if little-endian (Intel) */
+#define HOST_BIGENDIAN 1
+#else
+/* Set the native cpu bit order (FILLORDER_LSB2MSB or FILLORDER_MSB2LSB) */
+#define HOST_FILLORDER FILLORDER_LSB2MSB
+/* Native cpu byte order: 1 if big-endian (Motorola) or 0 if little-endian (Intel) */
+#undef WORDS_BIGENDIAN
+/* Native cpu byte order: 1 if big-endian (Motorola) or 0 if little-endian (Intel) */
 #undef HOST_BIGENDIAN
+#endif // BYTE_ORDER
 
 /* Support CCITT Group 3 & 4 algorithms */
-#undef CCITT_SUPPORT
+#define CCITT_SUPPORT 1
 
 /* Support JPEG compression (requires IJG JPEG library) */
-#undef JPEG_SUPPORT
+#define JPEG_SUPPORT 1
 
 /* Support JBIG compression (requires JBIG-KIT library) */
-#undef JBIG_SUPPORT
+/* #undef JBIG_SUPPORT */
 
 /* Support LogLuv high dynamic range encoding */
-#undef LOGLUV_SUPPORT
+#define LOGLUV_SUPPORT 1
 
 /* Support LZW algorithm */
-#undef LZW_SUPPORT
+#define LZW_SUPPORT 1
 
 /* Support NeXT 2-bit RLE algorithm */
-#undef NEXT_SUPPORT
+#define NEXT_SUPPORT 1
 
 /* Support Old JPEG compresson (read contrib/ojpeg/README first! Compilation
    fails with unpatched IJG JPEG library) */
-#undef OJPEG_SUPPORT
+#define OJPEG_SUPPORT 1
 
 /* Support Macintosh PackBits algorithm */
-#undef PACKBITS_SUPPORT
+#define PACKBITS_SUPPORT 1
 
 /* Support Pixar log-format algorithm (requires Zlib) */
-#undef PIXARLOG_SUPPORT
+#define PIXARLOG_SUPPORT 1
 
 /* Support ThunderScan 4-bit RLE algorithm */
-#undef THUNDER_SUPPORT
+#define THUNDER_SUPPORT 1
 
 /* Support Deflate compression */
-#undef ZIP_SUPPORT
+#define ZIP_SUPPORT 1
+
+/* Support LZMA2 compression */
+#undef LZMA_SUPPORT
+
+/* Support Microsoft Document Imaging format */
+#undef MDI_SUPPORT
 
 /* Support strip chopping (whether or not to convert single-strip uncompressed
    images to mutiple strips of ~8Kb to reduce memory usage) */
-#undef STRIPCHOP_DEFAULT
+#define STRIPCHOP_DEFAULT TIFF_STRIPCHOP
 
 /* Enable SubIFD tag (330) support */
-#undef SUBIFD_SUPPORT
+#define SUBIFD_SUPPORT 1
 
 /* Treat extra sample as alpha (default enabled). The RGBA interface will
    treat a fourth sample with no EXTRASAMPLE_ value as being ASSOCALPHA. Many
    packages produce RGBA files but don't mark the alpha properly. */
-#undef DEFAULT_EXTRASAMPLE_AS_ALPHA
+#define DEFAULT_EXTRASAMPLE_AS_ALPHA 1
 
 /* Pick up YCbCr subsampling info from the JPEG data stream to support files
    lacking the tag (default enabled). */
-#undef CHECK_JPEG_YCBCR_SUBSAMPLING
+#define CHECK_JPEG_YCBCR_SUBSAMPLING 1
 
 /* Support MS MDI magic number files as TIFF */
-#undef MDI_SUPPORT
+/* #undef MDI_SUPPORT */
 
 /*
  * Feature support definitions.
