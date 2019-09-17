fd1e950a937b7baa233ba313e831aa3fe7e4d2ab
Dinand Vanvelzen <dinand@vanvelzen.us>
2019-09-16 22:41:37 -0500

    Adding initial service macro. Needs work wrt calling normal app init logic

--- common/include/gucef_osmain.h
+++ common/include/gucef_osmain.h
@@ -73,15 +73,121 @@
                                               
     #define GUCEF_OSMAIN_END }
 
+    #define GUCEF_OSSERVICEMAIN_BEGIN( serviceName )                                                   \
+                                                                                                       \
+    ::SERVICE_STATUS_HANDLE g_win32ServiceStatusHandle = NULL;                                         \
+    ::HANDLE g_win32StopEvent = NULL;                                                                  \
+    ::DWORD g_win32CurrentState = 0;                                                                   \
+    bool g_win32SystemShutdown = false;                                                                \
+                                                                                                       \
+    void WINAPI Win32ServiceMain( DWORD argc, LPTSTR* argv );                                          \
+                                                                                                       \
+    int                                                                                                \
+    main( int argc, char* argv[] )                                                                     \
+    {                                                                                                  \
+        ::SERVICE_TABLE_ENTRY win32ServiceTable[] = {                                                  \
+            { "", &Win32ServiceMain },                                                                 \
+            { NULL, NULL }                                                                             \
+        };                                                                                             \
+                                                                                                       \
+        if ( ::StartServiceCtrlDispatcher( win32ServiceTable ) )                                       \
+            return 0;                                                                                  \
+        else if ( ERROR_FAILED_SERVICE_CONTROLLER_CONNECT == ::GetLastError() )                        \
+            return -1;                                                                                 \
+        else                                                                                           \
+            return -2;                                                                                 \
+    }                                                                                                  \
+                                                                                                       \
+    void                                                                                               \
+    Win32ReportErrorStatus( DWORD errorCode )                                                          \
+    {                                                                                                  \
+        g_win32CurrentState = SERVICE_STOPPED;                                                         \
+        SERVICE_STATUS serviceStatus = {                                                               \
+            SERVICE_WIN32_OWN_PROCESS,                                                                 \
+            g_win32CurrentState,                                                                       \
+            0,                                                                                         \
+            ERROR_SERVICE_SPECIFIC_ERROR,                                                              \
+            errorCode,                                                                                 \
+            0,                                                                                         \
+            0,                                                                                         \
+        };                                                                                             \
+        ::SetServiceStatus( g_win32ServiceStatusHandle, &serviceStatus );                              \
+    }                                                                                                  \
+                                                                                                       \
+    void                                                                                               \
+    Win32ReportStatus( DWORD state )                                                                   \
+    {                                                                                                  \
+        g_win32CurrentState = state;                                                                   \
+        SERVICE_STATUS serviceStatus = {                                                               \
+            SERVICE_WIN32_OWN_PROCESS,                                                                 \
+            g_win32CurrentState,                                                                       \
+            state == SERVICE_START_PENDING ? 0 : SERVICE_ACCEPT_STOP | SERVICE_ACCEPT_SHUTDOWN,        \
+            NO_ERROR,                                                                                  \
+            0,                                                                                         \
+            0,                                                                                         \
+            0,                                                                                         \
+        };                                                                                             \
+        ::SetServiceStatus( g_win32ServiceStatusHandle, &serviceStatus );                              \
+    }                                                                                                  \
+                                                                                                       \
+    DWORD WINAPI Win32HandlerEx( DWORD control, DWORD eventType, void* eventData, void* context )      \
+    {                                                                                                  \
+        switch ( control )                                                                             \
+        {                                                                                              \
+            case SERVICE_CONTROL_SHUTDOWN:                                                             \
+            {                                                                                          \
+                g_win32SystemShutdown = true;                                                          \
+                break;                                                                                 \
+            }                                                                                          \
+            case SERVICE_CONTROL_STOP:                                                                 \
+            {                                                                                          \
+                Win32ReportStatus( SERVICE_STOP_PENDING );                                             \
+                ::SetEvent( g_win32StopEvent );                                                        \
+                break;                                                                                 \
+            }                                                                                          \
+            default:                                                                                   \
+            {                                                                                          \
+                Win32ReportStatus( g_win32CurrentState );                                              \
+                break;                                                                                 \
+            }                                                                                          \
+        }                                                                                              \
+        return NO_ERROR;                                                                               \
+    }                                                                                                  \
+                                                                                                       \
+    void WINAPI Win32ServiceMain( DWORD argc, LPTSTR *argv )                                           \
+    {                                                                                                  \
+        g_win32ServiceStatusHandle =                                                                   \
+            ::RegisterServiceCtrlHandlerEx( serviceName, &Win32HandlerEx, NULL);                       \
+                                                                                                       \
+        Win32ReportStatus( SERVICE_START_PENDING );                                                    \
+        g_win32StopEvent = ::CreateEvent( NULL, TRUE, FALSE, NULL );                                   \
+                                                                                                       \
+        auto& application = ::GUCEF::CORE::CCoreGlobal::Instance()->GetApplication();                  \
+        int appResult = application.main( (int)argc, (char**)argv, true );                             \
+                                                                                                       \
+        ::CloseHandle( g_win32StopEvent );                                                             \
+                                                                                                       \
+        if ( appResult != 0 )                                                                          \
+            Win32ReportErrorStatus( (DWORD)appResult );                                                \
+        else                                                                                           \
+            Win32ReportStatus( SERVICE_STOPPED );                                                      \
+                                                                                                       \
+    }
+
+    #define GUCEF_OSSERVICEMAIN_END }
+
 #else
 
     #define GUCEF_OSMAIN_BEGIN                \
                                               \
     int                                       \
-    main( int argc , char* argv[] )           \
+    main( int argc, char* argv[] )            \
     {
     
     #define GUCEF_OSMAIN_END }
+
+    #define GUCEF_OSSERVICEMAIN_BEGIN( serviceName )    GUCEF_OSMAIN_BEGIN
+    #define GUCEF_OSSERVICEMAIN_END                     GUCEF_OSMAIN_END
 
 #endif
 
