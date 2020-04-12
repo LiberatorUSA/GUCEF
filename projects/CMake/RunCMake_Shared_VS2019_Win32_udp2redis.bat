8969e35dd4915b51a9a2eb820fffee3196bd1069
Dinand Vanvelzen <dinand@vanvelzen.us>
2020-04-12 01:10:05 -0500

    CMake: Regenerated targets after alteration to ProjectGenerator to compensate for CPack bug. Aparently CPack bugs out when getting a list of files to package into a package due to a regex filter that is applied. The regex filter was tripping on the use of _[exc]_ and _[tag]_ in the auto generated filenames. The ProjectGenerator code was adjusted to not add the [ ] anymore which worked around the issue.

--- projects/CMake/RunCMake_Shared_VS2019_Win32_udp2redis.bat
+++ projects/CMake/RunCMake_Shared_VS2019_Win32_udp2redis.bat
@@ -2,7 +2,7 @@ CALL CMakeCommon.bat
 
 ECHO *** Set VS2019 specifics and run CMake ***
 
-SET MAINCMAKE=%SRCROOTDIR%\projects\CMake\targets\GUCEF_[exe]_udp2redis
+SET MAINCMAKE=%SRCROOTDIR%\projects\CMake\targets\GUCEF_exe_udp2redis
 SET VS19_OUTPUTDIR="%OUTPUTDIR%\VS2019_udp2redis"
 
 CMake.exe -DBUILD_SHARED_LIBS=ON -G"Visual Studio 16 2019" -A Win32 -H"%MAINCMAKE%" -B%VS19_OUTPUTDIR%
