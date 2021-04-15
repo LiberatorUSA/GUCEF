b43b494c0e1a4ab40537057dadd483b4d4645dbe
Dinand Vanvelzen <dinand.vanvelzen@gmail.com>
2021-04-15 00:11:10 -0500

    draft aws lambda glue module skeleton

---
+++ plugins/SHARED/pluginglueAWSLAMBDARUNTIME/src/pluginglueAWSLAMBDARUNTIME.cpp
@@ -0,0 +1,125 @@
+/*
+ *  pluginglueAWSLAMBDARUNTIME: Library to push AWS Lambda work into the platform
+ *
+ *  Copyright (C) 1998 - 2020.  Dinand Vanvelzen
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+/*-------------------------------------------------------------------------//
+//                                                                         //
+//      INCLUDES                                                           //
+//                                                                         //
+//-------------------------------------------------------------------------*/
+
+#ifndef GUCEF_CORE_LOGGING_H
+#include "gucefCORE_Logging.h"
+#define GUCEF_CORE_LOGGING_H
+#endif /* GUCEF_CORE_LOGGING_H ? */
+
+#ifndef GUCEF_CORE_CTASKMANAGER_H
+#include "gucefCORE_CTaskManager.h"
+#define GUCEF_CORE_CTASKMANAGER_H
+#endif /* GUCEF_CORE_CTASKMANAGER_H ? */
+
+#ifndef GUCEF_CORE_CCOREGLOBAL_H
+#include "gucefCORE_CCoreGlobal.h"
+#define GUCEF_CORE_CCOREGLOBAL_H
+#endif /* GUCEF_CORE_CCOREGLOBAL_H ? */
+
+#include <aws/lambda-runtime/runtime.h>
+
+#include "pluginglueAWSLAMBDARUNTIME.h"
+
+/*-------------------------------------------------------------------------//
+//                                                                         //
+//      NAMESPACE                                                          //
+//                                                                         //
+//-------------------------------------------------------------------------*/
+
+namespace GUCEF {
+namespace PLUGINGLUE {
+namespace AWSLAMBDARUNTIME {
+
+/*-------------------------------------------------------------------------//
+//                                                                         //
+//      UTILITIES                                                          //
+//                                                                         //
+//-------------------------------------------------------------------------*/
+
+CORE::Int32 GUCEF_PLUGIN_CALLSPEC_PREFIX
+GUCEFPlugin_Load( CORE::UInt32 argc, const char** argv ) GUCEF_PLUGIN_CALLSPEC_SUFFIX
+{GUCEF_TRACE;
+
+    GUCEF_LOG( CORE::LOGLEVEL_NORMAL, "Load called on glue plugin AWSLAMBDARUNTIME" );
+
+    // When executing as a Lambda, which is the only reason for using this glue module, we want to allow the main thread
+    // to pick up AWS Lambda workload as well since it wont have anything else to do and we want to easily support 
+    // single-threaded Lambda code for ease of use
+    CORE::CCoreGlobal::Instance()->GetTaskManager().GetThreadPool()->SetAllowMainApplicationThreadToPickUpQueuedTasks( true );
+    
+    GUCEF_LOG( CORE::LOGLEVEL_NORMAL, "Load finished for glue plugin AWSLAMBDARUNTIME" );
+    return 1;
+}
+
+/*--------------------------------------------------------------------------*/
+
+void GUCEF_PLUGIN_CALLSPEC_PREFIX
+GUCEFPlugin_Unload( void ) GUCEF_PLUGIN_CALLSPEC_SUFFIX
+{GUCEF_TRACE;
+
+    GUCEF_LOG( CORE::LOGLEVEL_NORMAL, "Unload called on glue plugin AWSLAMBDARUNTIME" );
+}
+
+/*--------------------------------------------------------------------------*/
+
+void GUCEF_PLUGIN_CALLSPEC_PREFIX
+GUCEFPlugin_GetVersion( CORE::TVersion* versionInfo ) GUCEF_PLUGIN_CALLSPEC_SUFFIX
+{GUCEF_TRACE;
+
+    versionInfo->major = 0;
+    versionInfo->minor = 0;
+    versionInfo->patch = 0;
+    versionInfo->release = 0;
+}
+
+/*--------------------------------------------------------------------------*/
+
+const char* GUCEF_PLUGIN_CALLSPEC_PREFIX
+GUCEFPlugin_GetCopyright( void ) GUCEF_PLUGIN_CALLSPEC_SUFFIX
+{GUCEF_TRACE;
+
+    return "Copyright (C) Dinand Vanvelzen, Apache License v2";
+}
+
+/*--------------------------------------------------------------------------*/
+
+const char* GUCEF_PLUGIN_CALLSPEC_PREFIX
+GUCEFPlugin_GetDescription( void ) GUCEF_PLUGIN_CALLSPEC_SUFFIX
+{GUCEF_TRACE;
+
+    return "Generic GUCEF glue plugin for running work loads as Lambda functions using cloud provider AWS";
+}
+
+/*-------------------------------------------------------------------------//
+//                                                                         //
+//      NAMESPACE                                                          //
+//                                                                         //
+//-------------------------------------------------------------------------*/
+
+}; /* namespace AWSLAMBDARUNTIME */
+}; /* namespace PLUGINGLUE */
+}; /* namespace GUCEF */
+
+/*--------------------------------------------------------------------------*/
+
---
+++ projects/CMake/RunCMake_Shared_VS2019_Win64.bat
@@ -0,0 +1,9 @@
+CALL CMakeCommon.bat
+
+ECHO *** Set VS2019 specifics and run CMake ***
+
+SET MAINCMAKE=%SRCROOTDIR%\projects\CMake\targets\GUCEF
+SET VS19_OUTPUTDIR="%OUTPUTDIR%\VS2019_x64"
+
+CMake.exe -DBUILD_SHARED_LIBS=ON -G"Visual Studio 16 2019" -A x64 -H"%MAINCMAKE%" -B%VS19_OUTPUTDIR%
+PAUSE
