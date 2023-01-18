f7b6721b547f8e6f2b53f25be49dafc805f34123
Dinand Vanvelzen <dinand.vanvelzen@gmail.com>
2023-01-17 22:43:13 -0600

    AWS SDK: S3 and SQS modules are now compiling as well. working on compiling plugins using the latest AWS SDK

--- plugins/SHARED/pluginglueAWSSDK/src/pluginglueAWSSDK_CClientConfiguration.cpp
+++ plugins/SHARED/pluginglueAWSSDK/src/pluginglueAWSSDK_CClientConfiguration.cpp
@@ -144,7 +144,7 @@ CClientConfiguration::LoadConfig( const CORE::CDataNode& treeroot )
     verifySSL = CORE::StringToBool( treeroot.GetAttributeValueOrChildValueByName( m_settingsPrefix + "verifySSL", CORE::BoolToString( verifySSL ) ) );
     caPath = treeroot.GetAttributeValueOrChildValueByName( m_settingsPrefix + "caPath", caPath ).AsUtf8String();
     caFile = treeroot.GetAttributeValueOrChildValueByName( m_settingsPrefix + "caFile", caFile ).AsUtf8String();
-    followRedirects = (Aws::Client::FollowRedirectsPolicy) CORE::StringToBool( treeroot.GetAttributeValueOrChildValueByName( m_settingsPrefix + "followRedirects", CORE::Int32ToString( (CORE::Int32) followRedirects ) ) );
+    followRedirects = (Aws::Client::FollowRedirectsPolicy) CORE::StringToInt32( treeroot.GetAttributeValueOrChildValueByName( m_settingsPrefix + "followRedirects", CORE::Int32ToString( (CORE::Int32) followRedirects ) ) );
     disableExpectHeader = CORE::StringToBool( treeroot.GetAttributeValueOrChildValueByName( m_settingsPrefix + "disableExpectHeader", CORE::BoolToString( disableExpectHeader ) ) );
     enableClockSkewAdjustment = CORE::StringToBool( treeroot.GetAttributeValueOrChildValueByName( m_settingsPrefix + "enableClockSkewAdjustment", CORE::BoolToString( enableClockSkewAdjustment ) ) );
     enableHostPrefixInjection = CORE::StringToBool( treeroot.GetAttributeValueOrChildValueByName( m_settingsPrefix + "enableHostPrefixInjection", CORE::BoolToString( enableHostPrefixInjection ) ) );
